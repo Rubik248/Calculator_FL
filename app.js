c509e7c64cf11e6e56c515a95aac5c830175bc2e
Rubik <babichnr@bk.ru>
2023-07-07 11:37:43 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+
+
+
+
+
+
+const count = document.querySelector('.count')
+const country = document.querySelector('.country')
+const price = document.querySelector('.price')
+const logo = document.querySelector('.logo')
+
+const btn = document.querySelector('.btn')
+
+btn.addEventListener('click', event => {
+    const countValue = count.value
+    const countryValue = country.value
+    const priceValue = price.value
+    const logoValue = logo.value
+    const wrapper = document.querySelector('.cost')
+    if (countryValue == "UK" && logoValue == "yes") {
+        const costFin = 35 + countValue * 1.5
+        const deliveryCost = priceValue * 0.52
+        wrapper.innerHTML = costFin + deliveryCost + priceValue
+       
+    }
+})
+
+
+
+
